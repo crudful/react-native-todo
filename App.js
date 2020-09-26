db661fd0e80461da4b1efe9fa83dbdbbe5333394
Abdullah DURSUN <adursuns@gmail.com>
2020-09-26 14:29:14 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <Text>Hello from Crudful!</Text>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({});
+
+export default App;
