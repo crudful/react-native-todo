535a84e6bb71f437f45986c4d8b005115d158171
Abdullah DURSUN <adursuns@gmail.com>
2020-09-27 12:40:53 +0300

    Add exception handling to service methods

--- src/utils/todoService.js
+++ src/utils/todoService.js
@@ -1,22 +1,30 @@
 const cfAccessKey = process.env.CFACCESSKEY;
 
 const getTasks = async () => {
-  let response = await fetch('https://todo.crudful.com/tasks', {
-    method: 'GET',
-    headers: {cfAccessKey: cfAccessKey},
-  });
-  let json = await response.json();
-  return json.results;
+  try {
+    let response = await fetch('https://todo.crudful.com/tasks', {
+      method: 'GET',
+      headers: {cfAccessKey: cfAccessKey},
+    });
+    let json = await response.json();
+    return json.results;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const updateTask = async (id, data) => {
-  let response = await fetch(`https://todo.crudful.com/tasks/${id}`, {
-    method: 'PATCH',
-    headers: {cfAccessKey: cfAccessKey, 'Content-Type': 'application/json'},
-    body: JSON.stringify(data),
-  });
-  let task = await response.json();
-  return task;
+  try {
+    let response = await fetch(`https://todo.crudful.com/tasks/${id}`, {
+      method: 'PATCH',
+      headers: {cfAccessKey: cfAccessKey, 'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    });
+    let task = await response.json();
+    return task;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export {getTasks, updateTask};
